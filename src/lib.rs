851708b184c83423a13374410bb6505457e4e938
Adanos020 <adanos020@gmail.com>
2022-08-13 19:24:45 +0100

    Update crate documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -6,65 +6,33 @@
 //!
 //! ## Usage
 //!
-//! First, create your context type and your tab widget:
+//! First, construct the initial tree:
 //!
 //! ```rust
-//! use egui::{Frame, Ui, style::Margin};
-//! use egui_dock::Tab;
+//! use egui::style::Margin;
+//! use egui_dock::{TabBuilder, Tree};
 //!
-//! struct MyContext;
-//!
-//! struct MyTab {
-//!     text: String,
-//! }
-//!
-//! impl MyTab {
-//!     fn new(text: impl ToString) -> Self {
-//!         Self {
-//!             text: text.to_string(),
-//!         }
-//!     }
-//! }
-//!
-//! impl Tab<MyContext> for MyTab {
-//!     fn title(&self) -> &str {
-//!         &self.title
-//!     }
-//!
-//!     fn ui(&mut self, ui: &mut Ui, _ctx: &mut MyContext) {
-//!         let margin = Margin::same(4.0);
-//!
-//!         Frame::none().inner_margin(margin).show(ui, |ui| {
-//!             ui.label(&self.text);
-//!         });
-//!     }
-//! }
-//! ```
-//!
-//! Then construct the initial tree using your tab widget:
-//!
-//! ```rust
-//! use egui_dock::{NodeIndex, Tree};
-//!
-//! let tab1 = Box::new(MyTab::new("Tab 1"));
-//! let tab2 = Box::new(MyTab::new("Tab 2"));
-//! let tab3 = Box::new(MyTab::new("Tab 3"));
-//! let tab4 = Box::new(MyTab::new("Tab 4"));
-//! let tab5 = Box::new(MyTab::new("Tab 5"));
-//!
+//! let tab1 = TabBuilder::default()
+//!     .title("Tab 1")
+//!     .content(|ui| {
+//!         ui.label("Tab 1");
+//!     })
+//!     .build();
+//! let tab2 = TabBuilder::default()
+//!     .title("Tab 2")
+//!     .inner_margin(Margin::same(4.0))
+//!     .content(|ui| {
+//!         ui.label("Tab 2");
+//!     })
+//!     .build();
 //! let mut tree = Tree::new(vec![tab1, tab2]);
-//!
-//! // You can modify the tree in runtime
-//! let [a, b] = tree.split_left(NodeIndex::root(), 0.3, vec![tab3]);
-//! let [_, _] = tree.split_below(a, 0.7, vec![tab4]);
-//! let [_, _] = tree.split_below(b, 0.5, vec![tab5]);
 //! ```
 //!
-//! Finally, you can show the tree.
+//! Then, you can show the tree.
 //!
 //! ```rust
 //! let id = ui.id();
-//! egui_dock::show(&mut ui, id, style, tree, context);
+//! egui_dock::show(&mut ui, id, &style, &mut tree)
 //! ```
 
 mod style;
