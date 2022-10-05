fbc31db129ec3098245ca916ccfe7c5eb923724d
jacob <RegenJacob@gmx.de>
2022-10-05 21:23:43 +0200

    Make ScrollArea optional via Style

--- src/lib.rs
+++ src/lib.rs
@@ -467,21 +467,32 @@ impl<'tree, Tab> DockArea<'tree, Tab> {
 
                     let mut ui = ui.child_ui(rect, Default::default());
                     ui.push_id(node_index, |ui| {
-                        ScrollArea::both()
-                            .id_source(
-                                self.id
+                        if style.tab_include_scrollarea {
+                            ScrollArea::both()
+                                .id_source(
+                                    self.id
                                     .with((tab_viewer.title(tab).text(), "egui_dock::Tab")),
-                            )
-                            .show(ui, |ui| {
-                                Frame::none().inner_margin(tab_viewer.inner_margin()).show(
-                                    ui,
-                                    |ui| {
-                                        let available_rect = ui.available_rect_before_wrap();
-                                        ui.expand_to_include_rect(available_rect);
-                                        tab_viewer.ui(ui, tab);
-                                    },
+                                    )
+                                .show(ui, |ui| {
+                                    Frame::none().inner_margin(tab_viewer.inner_margin()).show(
+                                        ui,
+                                        |ui| {
+                                            let available_rect = ui.available_rect_before_wrap();
+                                            ui.expand_to_include_rect(available_rect);
+                                            tab_viewer.ui(ui, tab);
+                                        },
+                                        );
+                                });
+                        } else {
+                            Frame::none().inner_margin(tab_viewer.inner_margin()).show(
+                                ui,
+                                |ui| {
+                                    let available_rect = ui.available_rect_before_wrap();
+                                    ui.expand_to_include_rect(available_rect);
+                                    tab_viewer.ui(ui, tab);
+                                },
                                 );
-                            });
+                        }
                     });
                 }
 
