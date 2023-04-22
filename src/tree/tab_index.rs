5397d79bfe743542314a7a54521c0ff1ccabaaac
Adam Gąsior <adanos020@gmail.com>
2023-04-22 16:16:50 +0100

    Big refactor (#123)
    * Split everything into smaller files.
    
    * Move functions that should be in  from
    
    * Fix docs
    
    * Fix clippy
    
    * Fix docs again
    
    * Make clean doc comments without unnecessary imports
    
    * Part 1 of splitting up `DockArea::show_inside`
    
    * Part 2 of splitting up `DockArea::show_inside`
    
    * Part 3 of splitting up `DockArea::show_inside`
    
    * Part 4 of splitting up `DockArea::show_inside`
    
    * Clean-up, fix cursor icon on tab hover.
    
    * Fix tab title colour
    
    * Un-swap `is_active` and `is_focused` flags

---
+++ src/tree/tab_index.rs
@@ -0,0 +1,11 @@
+﻿/// Identifies a tab within a [`Node`](crate::Node).
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub struct TabIndex(pub usize);
+
+impl From<usize> for TabIndex {
+    #[inline]
+    fn from(index: usize) -> Self {
+        TabIndex(index)
+    }
+}
---
+++ src/widgets/mod.rs
@@ -0,0 +1,10 @@
+/// Container for dockable tabs.
+pub mod dock_area;
+
+pub(crate) mod popup;
+
+/// Trait for tab-viewing types.
+pub mod tab_viewer;
+
+pub use dock_area::DockArea;
+pub use tab_viewer::TabViewer;
