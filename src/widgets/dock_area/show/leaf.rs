5c261830c81d267e3f735e796ca4c4bf74e6783b
12089897411 <13019317170@163.com>
2023-08-24 09:39:53 +0800

    pdate tab's scroll

--- src/widgets/dock_area/show/leaf.rs
+++ src/widgets/dock_area/show/leaf.rs
@@ -761,7 +761,7 @@ impl<'tree, Tab> DockArea<'tree, Tab> {
                 tabs_style.tab_body.stroke,
             );
 
-            if self.scroll_area_in_tabs {
+            if self.scroll_area_in_tabs && tab_viewer.tab_scroll(tab) {
                 ScrollArea::both().show(ui, |ui| {
                     Frame::none()
                         .inner_margin(tabs_style.tab_body.inner_margin)
